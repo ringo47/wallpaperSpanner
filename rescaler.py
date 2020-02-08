b5a8e83051f41cbc086024b8f3a88c7f040a8165
Varshith Solipuram <vsolipuram5@gmail.com>
2020-02-08 16:42:28 -0500

    Generic scaler

--- rescaler.py
+++ rescaler.py
@@ -2,9 +2,10 @@
 import cv2
 import numpy as np
 import gi
-
+import os
 gi.require_version("Gdk", "3.0")
 from gi.repository import Gdk
+
 allmonitors = []
 gdkdsp = Gdk.Display.get_default()
 for i in range(gdkdsp.get_n_monitors()):
@@ -16,15 +17,20 @@ for i in range(gdkdsp.get_n_monitors()):
             geo.x, geo.y, geo.width, geo.height
         ]
     ])
-#print(allmonitors)
+    allmonitors[i].extend([monitor.get_width_mm(),monitor.get_height_mm()])
+print(allmonitors)
+def scaleFactor(x1,y1,x2,y2):
+    return (x1**2 + y1**2)**0.5/(x2**2 + y2**2)**0.5
 
-def spanned(monitorStatus):
-    #wp=cv2.imread('/home/ringo/Pictures/Wallpapers/cyberpunk-black-widow-4k-y0-2160x3840.jpg')
-    wp=cv2.imread('/home/ringo/Pictures/Wallpapers/cyberpunk-2077-black-widow-ve-2160x3840.jpg')
+def spanned(allmonitors):
+    monitorStatus=len(allmonitors)
+    #wp=cv2.imread(os.path.dirname(__file__)+'/cyberpunk-black-widow-4k-y0-2160x3840.jpg')
+    wp=cv2.imread(os.path.dirname(__file__)+'/cyberpunk-2077-black-widow-ve-2160x3840.jpg')
     pscale=0.9 #downscaling to 1920 from 2160
     bezel_offset=150
     topLeft=[500,0] #corner for cropping to a 1920x2160 window
-    scaling=1.51 #(24inch/15.6inch) upscale higher ppi monitor
+    scaling=scaleFactor(allmonitors[0][-1],allmonitors[0][-2],\
+                        allmonitors[1][-1],allmonitors[1][-2]) #returns 1.5153 for (24inch/15.6inch) upscale higher ppi monitor
 
     wp=cv2.resize(wp,(0,0),fx=pscale,fy=pscale,interpolation=cv2.INTER_AREA)
     wp=wp[topLeft[0]:topLeft[0]+2160, topLeft[1]:topLeft[1]+1920]
@@ -40,17 +46,16 @@ def spanned(monitorStatus):
     ##cv2.waitKey()
     ##cv2.imshow('wpt',wp_top)
     ##cv2.waitKey()
-
     ##cv2.imshow('wpt',nwp)
     ##cv2.waitKey()
     ##cv2.destroyAllWindows()
     
     if monitorStatus==2:
-        cv2.imwrite('/home/ringo/Pictures/Wallpapers/CyberWidow.jpg',nwp)
+        cv2.imwrite(os.path.dirname(__file__)+'/CyberWidow.jpg',nwp)
     else:
-        cv2.imwrite('/home/ringo/Pictures/Wallpapers/CyberWidow.jpg',wp_top)
+        cv2.imwrite(os.path.dirname(__file__)+'/CyberWidow.jpg',wp_top)
     
 
 if __name__=="__main__":
-    spanned(len(allmonitors))
+    spanned(allmonitors)
     
